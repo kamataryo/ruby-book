50f5c8474849f46bc132461ffe2e6c48d7722443
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2018-01-06 12:00:46 +0900

    Try module namespace

--- notes/module.rb
+++ notes/module.rb
@@ -9,7 +9,17 @@ class User
   extend Greeter
 end
 
+module NameSpace
+  class Product
+    def self.hello
+      'namespaced class hello'
+    end
+  end
+end
+
+
 user = User.new
 
 p user.hello
 p User.hello
+p NameSpace::Product.hello
